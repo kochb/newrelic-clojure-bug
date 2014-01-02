40106761bfe839ea9212aa9ea141e0ec1efe1f7e
Brad Koch <brad@farmlogs.com>
2014-01-01 22:34:27 -0500

    Using New Relic v3.2.0

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "New Relic error demo"
   :min-lein-version "2.0.0"
   :url "https://github.com/kochb/newrelic-clojure-bug"
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.2.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.1"]]
   :main newrelic.core)
